59e75db2c50400964fd894bdddcfd068e92c93a6
Vsevolod2001 <vsevolod.chistyakov@gmail.com>
2023-07-23 01:43:14 +0300

    trajecory class

---
+++ trajectory.py
@@ -0,0 +1,61 @@
+import numpy as np
+from constants import N_nod,a,d,n_att,D
+from Model import model
+from Model import basic_model
+import Value
+class trajectory:
+
+    def __init__(self,x,model=basic_model):
+        self.x=x
+        self.model=model
+    
+    def __str__(self):
+        return str(self.points) 
+
+    def markov(self):
+        mod=self.model
+        dS=0
+        currp=0
+        prevp=0
+        nextp=0
+        new=0
+        for j in range(N_nod):
+            i=np.random.randint(0,N_nod)
+            currp=self.x[i]
+            prevp=self.x[(i-1)%N_nod]
+            nextp=self.x[(i+1)%N_nod]
+            for k in range(n_att):
+                new=currp+(2*d)*np.random.rand()-d
+                dS = mod.S(prevp,new)-mod.S(prevp,currp)+mod.S(new,nextp)-mod.S(currp,nextp)
+                
+                if dS<=0:
+                    currp=new
+                else:
+                    r=np.random.rand()
+                    if r<np.exp(-dS):
+                        currp=new
+            self.x[i]=currp   
+        return 0         
+    
+    
+    def average(self,value):
+        Mean=0
+        Np=value.N_points
+        if Np==1:
+            for i in range(N_nod):
+                Mean+=value.func(self.x[i],self.model)
+        
+        if Np==2:
+            for i in range(N_nod):
+                Mean+=value.func( self.x[i%N_nod] , self.x[(i+1)%N_nod], self.model )
+        return Mean/N_nod
+    
+    def randgen(method="cold",model=basic_model):
+        
+        if method=="warm":
+            points=(2*D)*np.random.rand(N_nod)-D
+        
+        if method=="cold":
+            points=N_nod*[0]
+        
+        return trajectory(points,model) 
